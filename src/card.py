54a00421c82654ef267b6aceaf7462600b0533b8
Lama3415 <german.verr@mail.ru>
2024-10-01 14:31:58 +0300

    class Card

---
+++ src/card.py
@@ -0,0 +1,71 @@
+from typing import Self
+
+class LlamaCard:
+    VALUES = list(range(1, 7)) + [0]  # возможные значения карт (1-6 и 0 для "Ламы")
+    LLAMA = 0  # значение для карты "Лама"
+    def __init__(self, value: int):
+        """value (int): Номинальное значение карты."""
+        # инициализация карты с проверкой значения
+        if value not in LlamaCard.VALUES:
+            raise ValueError
+        self.value = value
+
+    def __repr__(self):
+        return str(self.value)
+
+    def __eq__(self, other):
+        # сравнение карт
+        if isinstance(other, int):
+            other = LlamaCard(value=other)
+        return self.value == other.value
+
+    def __lt__(self, other):
+        # порядок карт (1, 2, ..., 6, 0)
+        if self.value == other.value:
+            return False
+        if self.value == LlamaCard.LLAMA:
+            return False
+        if other.value == LlamaCard.LLAMA:
+            return True
+        return self.value < other.value
+
+    def save(self):
+        return repr(self)
+
+    @staticmethod
+    def load(text: str):
+        return LlamaCard(value=int(text))
+
+    def can_play_on(self, other: Self) -> bool:
+        # играть карту self на карту other
+        if self.value == other.value or self.value == other.value + 1:
+            return True
+        if self.value == LlamaCard.LLAMA:
+            return other.value == 6 or other.value == LlamaCard.LLAMA
+        if other.value == LlamaCard.LLAMA:
+            return self.value == 1 or self.value == LlamaCard.LLAMA
+        return False
+
+    @staticmethod
+    def all_cards():
+        # создание всех возможных карт
+        cards = [LlamaCard(value=val) for val in range(1, 7)]
+        cards.extend([LlamaCard(value=LlamaCard.LLAMA) for _ in range(8)])
+        return cards
+
+    def score(self, cards):
+        """
+        Возвращает очки карты на основе переданного списка карт.
+        Если карта является дубликатом (т.е. она появляется более одного раза в списке карт),
+        то очки равны 0. В противном случае очки равны номинальному значению карты.
+        :param cards: cards (list[Card]): Список карт для проверки на дубликаты.
+        :return: int: Очки карты.
+        """
+
+        # штрафные очки за карту
+        if self.value == LlamaCard.LLAMA:
+            return 10
+        elif cards.count(self) > 1:  # Если карта является дубликатом, возвращаем 0
+            return 0
+        else:  # В противном случае возвращаем номинальное значение карты
+            return self.value
