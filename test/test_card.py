b50ceda3383e82e38f4601dcc86036d64daa051b
Lama3415 <german.verr@mail.ru>
2024-10-29 14:48:03 +0300

    test hand

--- test/test_card.py
+++ test/test_card.py
@@ -1,80 +1,79 @@
-from src.card import LlamaCard
 import pytest
+from src.card import LlamaCard
+
 def test_init():
-    # тест инициализации карты
     c = LlamaCard(3)
     assert c.value == 3
 
-def test_save():
-    # тест сохранения карты
-    c = LlamaCard(3)
-    assert repr(c) == '3'
-    assert c.save() == '3'
-
-    c = LlamaCard(6)
-    assert repr(c) == '6'
-    assert c.save() == '6'
+@pytest.mark.parametrize("value, expected_repr", [
+    (3, '3'),
+    (6, '6'),
+    (1, '1'),
+    (0, 'Лама'),
+])
+def test_save(value, expected_repr):
+    c = LlamaCard(value)
+    assert repr(c) == expected_repr
+    assert c.save() == expected_repr
 
 def test_eq():
-    # тест сравнения двух карт
     c1 = LlamaCard(3)
     c2 = LlamaCard(3)
     c3 = LlamaCard(1)
     c4 = LlamaCard(2)
     c5 = LlamaCard(6)
+    c_lama = LlamaCard(0)  # Карта Лама
 
     assert c1 == c2
     assert c1 != c3
     assert c1 != c4
     assert c1 != c5
+    assert c_lama == LlamaCard(0)  # Проверка на равенство карты Лама
 
 def test_load():
-    # тест загрузки карты
     s = '3'
     c = LlamaCard.load(s)
     assert c == LlamaCard(3)
 
-    s = '6'
+    s = '0'  # Проверка загрузки карты Лама
     c = LlamaCard.load(s)
-    assert c == LlamaCard(6)
-
-def test_divzero():
-    # пример теста с ловлей исключения
-    with pytest.raises(ZeroDivisionError):
-        x = 2 / 0
+    assert c == LlamaCard(0)
 
 def test_validation():
-    # тест валидации карты
     with pytest.raises(ValueError):
-        LlamaCard('3')
+        LlamaCard('3')  # Проверка на невалидный ввод
 
 def test_play_on():
-    # тест метода can_play_on
     c1 = LlamaCard.load('1')
     c2 = LlamaCard.load('2')
     c3 = LlamaCard.load('3')
     c4 = LlamaCard.load('4')
+    c_lama = LlamaCard.load('0')  # Карта Лама
 
     assert c1.can_play_on(c1)
     assert c2.can_play_on(c1)
     assert c2.can_play_on(c2)
     assert not c3.can_play_on(c1)
     assert not c4.can_play_on(c1)
+    assert c_lama.can_play_on(c1)  # Проверка, что Лама может играть на 1
+    assert c_lama.can_play_on(c_lama)  # Проверка, что Лама может играть на Ламу
 
 def test_all_cards():
-    # тест метода all_cards
-    cards = LlamaCard.all_cards(value=[5, 2, 6])
+    cards = LlamaCard.all_cards(value=[5, 2, 6, 0])
     expected_cards = [
         LlamaCard.load('5'),
         LlamaCard.load('2'),
         LlamaCard.load('6'),
+        LlamaCard.load('0'),  # Проверка карты Лама
     ]
     assert cards == expected_cards
 
 def test_score():
-    # тест метода score
     c = LlamaCard(6)
-    assert 6 == c.score()
+    assert c.score() == 6
 
     c = LlamaCard(5)
-    assert 5 == c.score()
+    assert c.score() == 5
+
+    c_lama = LlamaCard(0)  # Проверка карты Лама
+    assert c_lama.score() == 10  # Теперь карта Лама имеет 10 очков
--- test/test_hand.py
+++ test/test_hand.py
@@ -1,8 +1,9 @@
 import random
-from src.card import LlamaCard
-from src.hand import Hand
+from card import LlamaCard
+from hand import Hand
 
-cards = [LlamaCard(3), LlamaCard(1), LlamaCard(6)]
+# Инициализация карт, включая карту Лама
+cards = [LlamaCard(3), LlamaCard(1), LlamaCard(6), LlamaCard(0)]  # 0 - карта Лама
 
 def test_init():
     d = Hand(cards=cards)
@@ -10,33 +11,36 @@ def test_init():
 
 def test_save():
     d = Hand(cards=cards)
-    assert d.save() == '3 1 6'
+    assert d.save() == '3 1 6 Лама'  # Предположим, что карта Лама представляется как 'Лама'
 
     d = Hand(cards=[])
     assert d.save() == ''
 
 def test_load():
-    d = Hand.load('3 1 6')
+    d = Hand.load('3 1 6 Лама')
     expected_deck = Hand(cards)
     assert d == expected_deck
 
 def test_score():
-    h = Hand.load('3 0 6')
-    assert h.score() == 19
+    h = Hand.load('3 1 6 Лама')
+    assert h.score() == 10 + 3 + 1 + 6  # 10 очков за Ламу, 10 в сумме
 
-    h = Hand.load('5 3')
-    assert h.score() == 8
+    h = Hand.load('5 4')
+    assert h.score() == 9
+
+    h = Hand.load('Лама')  # Проверка только карты Лама
+    assert h.score() == 10  # Убедитесь, что Лама дает 10 очков
 
 def test_add_card():
     h = Hand.load('3 1 6')
-    h.add_card(LlamaCard.load('6'))
-    assert repr(h) == '3 1 6 6'
+    h.add_card(LlamaCard.load('Лама'))  # Добавление карты Лама
+    assert repr(h) == '3 1 6 Лама'  # Проверка, что Лама добавлена
 
     h.add_card(LlamaCard.load('4'))
-    assert repr(h) == '3 1 6 6 4'
+    assert repr(h) == '3 1 6 Лама 4'
 
 def test_remove_card():
-    h = Hand.load('3 1 6 6 4')
-    c = LlamaCard.load('6')
+    h = Hand.load('3 1 6 Лама 4')
+    c = LlamaCard.load('Лама')  # Удаление карты Лама
     h.remove_card(c)
-    assert repr(h) == '3 1 6 4'
+    assert repr(h) == '3 1 6 4'  # Проверка, что Лама была успешно удалена
