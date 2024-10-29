c9a3493a922bff7acaf19a2306c852ed456b9850
Lama3415 <german.verr@mail.ru>
2024-10-29 14:17:53 +0300

    hand and test_hand

--- src/hand.py
+++ src/hand.py
@@ -1,40 +1,44 @@
 import typing
-from src.card import LlamaCard
+
+from card import LlamaCard
 
 class Hand:
-    def __init__(self, value: list[LlamaCard] = []):
-        # Конструктор, принимающий список карт. По умолчанию - пустой список.
-        if value is None:
+    def __init__(self, cards:  list[LlamaCard] = []):
+        if cards is None:
             cards = []
+        self.cards: list[LlamaCard] = cards
 
     def __repr__(self):
-        # Возвращаем строковое представление объекта, используя метод save
         return self.save()
 
     def save(self) -> str:
-        # Преобразуем каждую карту в строку и объединяем их
-        scards=[c.save() for c in self.value]
-        s = ' '.join(scards)  # Объединяем список строк в одну строку return s  # Возвращаем строку формата '3 1 6'
+        """Convert deck to string in '3 1 6' format."""
+        scards = [c.save() for c in self.cards]         # ['3', '1', '6']
+        s = ' '.join(scards)
         return s
+
     def __eq__(self, other):
-        # Сравниваем текущий объект Hand с другим объектом
-        if isinstance(other, str): #Если другой объект - строка, загружаем его как объект Hand other = Hand.load(other)
-            return self.value == other.value  # Сравниваем списки карт def add_card(self, card: LlamaCard):
-            # Добавляем карту в список карт
-            self.value.append(card)
+        if isinstance(other, str):
+            other = Hand.load(other)
+        return self.cards == other.cards
+
+
+    def add_card(self, card: LlamaCard):
+        self.cards.append(card)
 
     @classmethod
     def load(cls, text: str) -> typing.Self:
-        # Преобразуем строку в список карт и создаем объект Hand
-        cards = [LlamaCard.load(s) for s in text.split()]  # Разбиваем строку и загружаем карты
-        return cls(value=cards)  # Возвращаем новый объект Hand
+        """Convert string in '3 1 6' format to Deck. Return deck."""
+        cards = [LlamaCard.load(s) for s in text.split()]
+        return cls(cards=cards)
 
     def remove_card(self, card: LlamaCard):
-        # Удаляем карту из списка карт
-        self.value.remove(card)
+        self.cards.remove(card)
 
     def score(self):
+        """Штрафные очки"""
         res = 0
         for c in self.cards:
             res += c.score()
         return res
+
