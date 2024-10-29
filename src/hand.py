4f144b503c6780838537f967d2b8a3821f358d68
Lama3415 <german.verr@mail.ru>
2024-10-29 14:37:55 +0300

    Hand

--- src/hand.py
+++ src/hand.py
@@ -1,9 +1,9 @@
 import typing
-
 from card import LlamaCard
 
 class Hand:
-    def __init__(self, cards:  list[LlamaCard] = []):
+    def __init__(self, cards: list[LlamaCard] = None):
+        # Используем None вместо пустого списка по умолчанию, чтобы избежать проблем с изменяемыми объектами
         if cards is None:
             cards = []
         self.cards: list[LlamaCard] = cards
@@ -12,8 +12,8 @@ class Hand:
         return self.save()
 
     def save(self) -> str:
-        """Convert deck to string in '3 1 6' format."""
-        scards = [c.save() for c in self.cards]         # ['3', '1', '6']
+        """Преобразует руку в строку в формате '3 1 6'."""
+        scards = [c.save() for c in self.cards]  # ['3', '1', '6']
         s = ' '.join(scards)
         return s
 
@@ -22,23 +22,24 @@ class Hand:
             other = Hand.load(other)
         return self.cards == other.cards
 
-
     def add_card(self, card: LlamaCard):
+        """Добавляет карту в руку."""
         self.cards.append(card)
 
     @classmethod
     def load(cls, text: str) -> typing.Self:
-        """Convert string in '3 1 6' format to Deck. Return deck."""
+        """Преобразует строку в формате '3 1 6' в объект Hand. Возвращает руку."""
         cards = [LlamaCard.load(s) for s in text.split()]
         return cls(cards=cards)
 
     def remove_card(self, card: LlamaCard):
+        """Удаляет карту из руки."""
         self.cards.remove(card)
 
-    def score(self):
-        """Штрафные очки"""
+    def score(self) -> int:
+        """Возвращает сумму очков карт в руке."""
         res = 0
         for c in self.cards:
-            res += c.score()
+            res += c.score(self.cards)  # Передаем текущую руку для проверки дубликатов
         return res
 
