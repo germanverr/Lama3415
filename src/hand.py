aa92408d4378fdb4d45dc6f33306010a372cbdfd
Lama3415 <german.verr@mail.ru>
2024-11-26 11:10:25 +0300

    Исправление Hand и init

--- src/hand.py
+++ src/hand.py
@@ -3,24 +3,33 @@ from card import LlamaCard
 
 class Hand:
     def __init__(self, cards: list[LlamaCard] = None):
-        # Используем None вместо пустого списка по умолчанию, чтобы избежать проблем с изменяемыми объектами
         if cards is None:
             cards = []
         self.cards: list[LlamaCard] = cards
+
+    def playable_cards(self, top_card: LlamaCard) -> list[LlamaCard]:
+        """Возвращает список карт, которые можно сыграть на верхнюю карту."""
+        return [card for card in self.cards if self.can_play(card, top_card)]
+
+    def can_play(self, card: LlamaCard, top_card: LlamaCard) -> bool:
+        """Определяет, может ли карта быть сыграна на верхнюю карту."""
+        return card.value == top_card.value  # Убедитесь, что здесь логика соответствует правилам игры
 
     def __repr__(self):
         return self.save()
 
     def save(self) -> str:
         """Преобразует руку в строку в формате '3 1 6'."""
-        scards = [c.save() for c in self.cards]  # ['3', '1', '6']
-        s = ' '.join(scards)
-        return s
+        scards = [c.save() for c in self.cards]
+        return ' '.join(scards)
 
     def __eq__(self, other):
-        if isinstance(other, str):
+        if isinstance(other, Hand):
+            return self.cards == other.cards
+        elif isinstance(other, str):
             other = Hand.load(other)
-        return self.cards == other.cards
+            return self.cards == other.cards
+        return False
 
     def add_card(self, card: LlamaCard):
         """Добавляет карту в руку."""
@@ -33,13 +42,12 @@ class Hand:
         return cls(cards=cards)
 
     def remove_card(self, card: LlamaCard):
-        """Удаляет карту из руки."""
-        self.cards.remove(card)
+        """Удаляет карту из руки, если она там есть."""
+        try:
+            self.cards.remove(card)
+        except ValueError:
+            print(f"Card {card} not found in hand.")
 
     def score(self) -> int:
         """Возвращает сумму очков карт в руке."""
-        res = 0
-        for c in self.cards:
-            res += c.score(self.cards)  # Передаем текущую руку для проверки дубликатов
-        return res
-
+        return sum(c.score(self.cards) for c in self.cards)
--- src/player_interactions/init.py
+++ src/player_interactions/init.py
@@ -1,9 +1,12 @@
-# src/player_interactions/init.py
+# src/player_interactions/__init__.py
 
 from src.player_interactions.ai_player import Bot
 from src.player_interactions.human_player import Human
 from src.player_interactions.humanGUI_player import HumanGUI
 
-__all__ = ['Bot', 'Human', 'HumanGUI']
+# Определяем список всех классов игроков
+all_player_types = [Bot, Human, HumanGUI]
+
+__all__ = ['Bot', 'Human', 'HumanGUI', 'all_player_types']
 
 
